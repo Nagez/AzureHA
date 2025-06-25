554fcf8d2675ccbbf13f301e92c6bc6fa09f4411
Nagez <81879748+Nagez@users.noreply.github.com>
2025-06-26 00:24:11 +0300

    added network_policy = "azure" to aks terraform

--- startup.sh
+++ startup.sh
@@ -1,7 +1,17 @@
-#docker image
+# use acr terraform
+
+# use aks terraform
+
+# attach acr to aks
+az aks update -n service-a-cluster -g service-a-cluster-rg --attach-acr orserviceaacr123456
+
+#create and register docker image
 docker build -t orserviceaacr123456.azurecr.io/service-a:latest .
 az acr login --name orserviceaacr123456
 docker push orserviceaacr123456.azurecr.io/service-a:latest
+
+# go to cluster context
+az aks get-credentials --resource-group service-a-cluster-rg --name orn-ha-cluster 
 
 # adding ingress controller
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx 
@@ -15,11 +25,10 @@ kubectl apply -f base/access/service-b-access.yaml
 kubectl apply -f services/service-b/deployment.yaml
 kubectl apply -f services/service-b/service.yaml
 
-# attach acr to aks
-az aks update -n service-a-cluster -g service-a-cluster-rg --attach-acr orserviceaacr123456
-
 # adding service-a
-cd .\kubernetes\
 kubectl apply -f base/access/service-a-access.yaml
 kubectl apply -f services/service-a/deployment.yaml
 kubectl apply -f services/service-a/service.yaml
+
+# apply network policy
+kubectl apply -f .\base\networkpolicy\block-service-a-to-b.yaml  
